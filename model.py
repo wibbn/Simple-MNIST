5f95edffd8bfbdbcd67a3b4f3868c8c699d37b9e
wibbn <code.wibbn@gmail.com>
2020-02-01 17:45:47 +0300

    init

---
+++ model.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = 
